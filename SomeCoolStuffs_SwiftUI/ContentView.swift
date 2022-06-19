0814146c00a1121effb55bc074998f094123bf88
kneskoromny <kneskoromny@gmail.com>
2022-06-19 19:26:50 +0300

    Initial Commit

---
+++ SomeCoolStuffs_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SomeCoolStuffs_SwiftUI
+//
+//  Created by Кирилл Нескоромный on 19.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SomeCoolStuffs_SwiftUI/SomeCoolStuffs_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeCoolStuffs_SwiftUIApp.swift
+//  SomeCoolStuffs_SwiftUI
+//
+//  Created by Кирилл Нескоромный on 19.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SomeCoolStuffs_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
